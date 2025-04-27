1480b2c07d53a4cf27dc36ae34aaf49c6bb6c889
Fernando Costa Nardi <fernandocostanardi@gmail.com>
2025-04-27 14:43:19 -0300

    feat: adiciona BrapiClient e AssetFactory para separar responsabilidades de requisição e parsing de ativos

---
+++ backend/src/main/java/com/olisystem/optionsmanager/parser/AssetFactory.java
@@ -0,0 +1,22 @@
+package com.olisystem.optionsmanager.parser;
+
+import com.olisystem.optionsmanager.model.Asset;
+import com.olisystem.optionsmanager.model.AssetType;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class AssetFactory {
+  public static Asset fromJson(JSONObject json) throws Exception {
+    JSONArray results = json.getJSONArray("results");
+    if (results.length() > 0) {
+      JSONObject ativo = results.getJSONObject(0);
+      Asset info = new Asset();
+      info.setCode(ativo.getString("symbol"));
+      info.setName(ativo.optString("longName", "Nome desconhecido"));
+      info.setUrlLogo(ativo.optString("logourl", ""));
+      info.setType(AssetType.STOCK);
+      return info;
+    }
+    throw new Exception("Nenhum resultado encontrado no JSON para o ativo.");
+  }
+}
